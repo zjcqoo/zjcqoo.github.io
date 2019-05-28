3ab5c14a96365dda4b884a48665dce126d91489b
zjcqoo <zjcqoo@qq.com>
2019-05-28 10:35:33 +0800

    hk-1 流量已用完撤下

--- conf.js
+++ conf.js
@@ -9,7 +9,7 @@ jsproxy_config({
       lines: [
         // 多条线路，负载均衡系统会从其中选一条
         'node-aliyun-hk-0.etherdream.com:8443',
-        'node-aliyun-hk-1.etherdream.com:8443'
+        //'node-aliyun-hk-1.etherdream.com:8443'
       ]
     },
     'aliyun-sg': {
