f9a4b6dfc6f1cff3a39fa8b2b9f0bf583d628aa7
zjcqoo <zjcqoo@gmail.com>
2019-06-11 15:50:20 +0800

    auto sync by update.sh

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-importScripts(__FILE__='https://cdn.jsdelivr.net/gh/etherdream/jsproxy@gh-pages/assets/bundle.165e979e.js')
+importScripts(__FILE__='https://cdn.jsdelivr.net/gh/etherdream/jsproxy@gh-pages/assets/bundle.91ae9a07.js')
