bcf41e350728d11ceb0688f6a01f3bf810af2ad7
zjcqoo <zjcqoo@gmail.com>
2019-06-21 13:54:53 +0800

    auto sync by update.sh

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-jsproxy_config=x=>importScripts(__FILE__=x.assets_cdn+'bundle.d8cdf274.js');importScripts('conf.js')
+jsproxy_config=x=>{__CONF__=x;importScripts(__FILE__=x.assets_cdn+'bundle.03a61bde.js')};importScripts('conf.js')
