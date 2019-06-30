e1a34c86ee8fc05807375330c07eac96314f6284
zjcqoo <zjcqoo@gmail.com>
2019-06-30 20:57:54 +0800

    auto sync by update.sh

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
-  ver: '61',
+  ver: '62',
 
   static_boost: {
     enable: true,
@@ -37,7 +37,10 @@ jsproxy_config({
       hidden: true,
       lines: [
         // 实验中...
-        'shrill-unit-8594.jsproxy.workers.dev',
+        'a.jsproxy.workers.dev',
+        'a.007.workers.dev',
+        'a.hehe.workers.dev',
+        'a.lulu.workers.dev',
       ]
     }
   },
