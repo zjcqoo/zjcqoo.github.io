ee9fca58fe28a5b3247cbd2e1cbcc181b8fd5c7a
zjcqoo <zjcqoo@gmail.com>
2019-07-01 21:19:52 +0800

    auto sync by update.sh

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
-  ver: '74',
+  ver: '75',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -37,14 +37,14 @@ jsproxy_config({
       hidden: true,
       lines: {
         // 收费版（高权重）
-        'node-cfworker.etherdream.com': 6,
+        'node-cfworker.etherdream.com': 30,
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        'a.007.workers.dev': 1,
-        'a.hehe.workers.dev': 1,
-        'a.lulu.workers.dev': 1,
-        'shrill-unit-8594.jsproxy.workers.dev': 1,
+        'a.007.workers.dev': 3,
+        'a.hehe.workers.dev': 3,
+        'a.lulu.workers.dev': 2,
+        'shrill-unit-8594.jsproxy.workers.dev': 5,
       }
     }
   },
