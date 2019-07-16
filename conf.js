3d8637b95d2e524d0db5e034dd2cfbd7b6b5ae95
zjcqoo <zjcqoo@gmail.com>
2019-07-16 14:35:21 +0800

    auto sync by update.sh

--- conf.js
+++ conf.js
@@ -37,7 +37,7 @@ jsproxy_config({
       hidden: true,
       lines: {
         // 收费版（高权重）
-        'node-cfworker.etherdream.com': 6,
+        'node-cfworker.etherdream.com': 4,
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
