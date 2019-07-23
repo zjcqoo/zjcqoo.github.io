70502f701f52a9a6ce9bd591dd19ce64c1ee37bd
zjcqoo <zjcqoo@gmail.com>
2019-07-23 13:39:56 +0800

    auto sync by update.sh

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '102',
+  ver: '103',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -16,6 +16,7 @@ jsproxy_config({
       label: '演示服务-香港节点',
       lines: {
         // 主机:权重
+        'node-aliyun-hk-0.etherdream.com:8443': 1,
         'node-aliyun-hk-1.etherdream.com:8443': 1,
         'node-aliyun-hk-2.etherdream.com:8443': 1,
       }
